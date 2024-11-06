5f3a96949532f523bd89037d831da5851e9c46af
Benjamin Fattori <ben.fattori@mail.utoronto.ca>
2024-11-06 08:44:45 -0500

    updates to fused scan, use exp2f in places, don't alloc shifted delta tensor for bwd pass, reordering in a few places for better computation and data movement overlaps

--- mamba/scan_fused.py
+++ mamba/scan_fused.py
@@ -32,7 +32,7 @@ def softplus_fwd(x):
 
 @triton.jit
 def softplus_bwd(x):
-    return tl.exp(x) / (1 + tl.exp(x))
+    return tl.exp2(x) / (1 + tl.exp2(x))
 
 
 @triton.jit
@@ -52,6 +52,8 @@ def parallel_scan_fwd_kernel(
     BLOCKSIZE_L: tl.constexpr,
     N: tl.constexpr,
 ):
+
+    ln_2: tl.constexpr = 1.44269504
 
     # parallelize over (batch, channel) dimension
     b_pid = tl.program_id(0)
@@ -77,7 +79,6 @@ def parallel_scan_fwd_kernel(
         dtype=tl.float32,
     )
 
-    # backward pass needs to then reduce something somehow?
     for chan_idx in range(N):
 
         B_ch = tl.load(B_ptr + chan_idx * B_n_stride + l_offs).to(tl.float32)
@@ -88,7 +89,7 @@ def parallel_scan_fwd_kernel(
 
         A_ch = tl.load(A_ptr + chan_idx * A_col_stride).to(tl.float32)  # single value
 
-        deltaA_ch = tl.exp(A_ch * delta)  # [L]
+        deltaA_ch = tl.exp2(ln_2 * A_ch * delta)  # [L]
 
         _, o = tl.associative_scan(
             (deltaA_ch, deltaB_ch), axis=0, combine_fn=associative_binary_op
@@ -122,6 +123,9 @@ def parallel_scan_forward(
     if BLOCKSIZE_L > 1024:
         num_warps = 4
 
+    if BLOCKSIZE_L > 2048:
+        num_warps = 8
+
     # fmt: off
     parallel_scan_fwd_kernel[grid](
         A,B,C,delta,
@@ -144,16 +148,18 @@ def parallel_scan_bwd_kernel(
     A_ptr,B_ptr,C_ptr,delta_ptr,
     x_ptr,dout_ptr,
     dA_ptr,dB_ptr,dC_ptr,ddelta_ptr, 
-    dx_ptr, delta_shifted_ptr,
+    dx_ptr,
     A_row_stride,A_col_stride,
-    dA_b_stride,dA_row_stride,dA_col_stride,
+    dA_b_stride,dA_row_stride,
     B_b_stride,B_n_stride,
     dB_b_stride,dB_d_stride, dB_n_stride,
     x_b_stride, x_d_stride,
     BLOCKSIZE_L: tl.constexpr, # sequence length
     N: tl.constexpr, # state size
 ):
-    # fmt: on
+# fmt: on
+
+    ln_2: tl.constexpr = 1.44269504
 
     # parallelize over (batch, channel) dimension
     b_pid = tl.program_id(0)
@@ -167,9 +173,6 @@ def parallel_scan_bwd_kernel(
     dx_ptr += b_pid * x_b_stride + d_pid * x_d_stride
     ddelta_ptr += b_pid * x_b_stride + d_pid * x_d_stride
     delta_ptr += b_pid * x_b_stride + d_pid * x_d_stride
-
-
-    delta_shifted_ptr += b_pid * x_b_stride + d_pid * x_d_stride
     
     dout_ptr += b_pid * x_b_stride + d_pid * x_d_stride
 
@@ -178,21 +181,19 @@ def parallel_scan_bwd_kernel(
     x = tl.load(x_ptr + l_offs).to(tl.float32)  # [L]
     delta = softplus_fwd(tl.load(delta_ptr + l_offs).to(tl.float32))  # [L]
 
-    delta_shifted = softplus_fwd(tl.load(delta_shifted_ptr + l_offs).to(tl.float32))  # [L]
+    delta_shifted = softplus_fwd(tl.load(delta_ptr + 1 + l_offs, mask = l_offs < BLOCKSIZE_L, other=1.0).to(tl.float32))
 
     A_ptr += A_row_stride * d_pid
 
     C_ptr += b_pid * B_b_stride
     B_ptr += b_pid * B_b_stride
     
-
     dX = tl.zeros(
         [
             BLOCKSIZE_L,
         ],
         dtype=tl.float32,
     )
-
 
     ddelta = tl.zeros(
         [
@@ -206,15 +207,15 @@ def parallel_scan_bwd_kernel(
         B_ch = tl.load(B_ptr + chan_idx * B_n_stride + l_offs).to(tl.float32)
         deltaB_ch = delta * x * B_ch  # [L]
         A_ch = tl.load(A_ptr + chan_idx * A_col_stride).to(tl.float32)  # single value
-        deltaA_ch = tl.exp(A_ch * delta)  # [L]
+        deltaA_ch = tl.exp2(ln_2 * A_ch * delta)  # [L]
 
         dout = tl.load(dout_ptr + l_offs).to(tl.float32)
         C = tl.load(C_ptr + chan_idx * B_n_stride + l_offs).to(tl.float32)
 
+        # run forward scan to create hidden activation
         _, h_ch = tl.associative_scan( 
             (deltaA_ch, deltaB_ch), axis=0, combine_fn=associative_binary_op
         )
-
 
         C_ch_grad = dout * h_ch
 
@@ -222,11 +223,14 @@ def parallel_scan_bwd_kernel(
 
         out_h_grad = dout * C
 
-        # reverse scan
-
-        deltaA_ch_shifted = tl.exp(A_ch * delta_shifted)  # [L]
+        deltaA_ch_shifted = tl.exp2(ln_2 * A_ch * delta_shifted)  # [L]
         deltaA_ch_shifted = tl.where(tl.arange(0, BLOCKSIZE_L) < (BLOCKSIZE_L - 1), deltaA_ch_shifted, 0.0)
+
+        # run reverse scan to create hidden grad
         _, h_grad = tl.associative_scan((deltaA_ch_shifted, out_h_grad), axis=0, combine_fn=associative_binary_op, reverse=True)
+
+        # [0,h_0,h_1,...]
+        h_ch_rolled = roll(h_ch, dim = 0)
 
         dX += (h_grad * delta * B_ch)
 
@@ -234,22 +238,17 @@ def parallel_scan_bwd_kernel(
 
         tl.store(dB_ptr + (chan_idx*dB_n_stride) + l_offs, value=B_ch_grad.to(tl.bfloat16))
 
-        # need a way to create h_tilde from h_ch like: [0,h_0,h_1,...]
-        h_ch_rolled = roll(h_ch, dim = 0)
-
         intermediate = h_grad * h_ch_rolled * deltaA_ch
 
         ddelta += (intermediate * A_ch)
         ddelta += (h_grad * (B_ch * x))
 
-        # print(intermediate.shape, delta.shape)
         A_batched_grad = tl.sum(intermediate * delta)
 
         tl.store(dA_ptr + (chan_idx), value=A_batched_grad.to(tl.float32))
 
-    # kinda dumb but w.e.
     delta = tl.load(delta_ptr + l_offs).to(tl.float32)
-    ddelta = ddelta * softplus_bwd(delta)
+    ddelta = ddelta * softplus_bwd(ln_2*delta)
 
     tl.store(ddelta_ptr + l_offs, value= ddelta.to(tl.bfloat16))
     tl.store(dx_ptr + l_offs, value= dX.to(tl.bfloat16))
@@ -266,24 +265,28 @@ def parallel_scan_backward(A, delta, B, C, x, grad_output) -> torch.Tensor:
     
     # [b, d, n]
     A_batch_grad = torch.empty((b, A.shape[0], A.shape[1]), device=x.device)
-    
 
     B_grad = torch.empty((b, d, n, l), device=x.device, dtype = torch.bfloat16) # [b, d, n, l] 
     C_grad = torch.empty((b, d, n, l), device=x.device, dtype = torch.bfloat16) # [b, d, n, l] 
 
+    # these are fine to parallelize over easily
     delta_grad = torch.empty_like(delta) # [b,d,l]
     x_grad = torch.empty_like(x) # [b,d,l]
-    
+
+
     num_warps = 2
     if BLOCKSIZE_L > 1024:
         num_warps = 4 
+
+    if BLOCKSIZE_L > 2048:
+        num_warps = 8
     
-    delta_shifted = torch.cat([delta, torch.ones_like(delta[:, :, :1])], dim=-1)[:, :, 1:].contiguous()
+        
     parallel_scan_bwd_kernel[grid](
         A, B, C, delta, x, grad_output,
-        A_batch_grad, B_grad, C_grad, delta_grad, x_grad, delta_shifted,
-        A.stride(0), A.stride(1),
-        A_batch_grad.stride(0), A_batch_grad.stride(1), A_batch_grad.stride(2),
+        A_batch_grad, B_grad, C_grad, delta_grad, x_grad, 
+        A.stride(0),A.stride(1),
+        A_batch_grad.stride(0), A_batch_grad.stride(1),
         B.stride(0),B.stride(1), 
         B_grad.stride(0),B_grad.stride(1),B_grad.stride(2), 
         x.stride(0), x.stride(1),
@@ -292,22 +295,13 @@ def parallel_scan_backward(A, delta, B, C, x, grad_output) -> torch.Tensor:
         num_warps = num_warps
     )
 
-    return A_batch_grad.sum(dim=0), B_grad.sum(dim = 1), C_grad.sum(dim = 1), delta_grad, x_grad
+    return A_batch_grad.sum(dim=0).to(A.dtype), B_grad.sum(dim = 1), C_grad.sum(dim = 1), delta_grad, x_grad
 
 
 class MambaRecurrence(Function):
     @staticmethod
     @torch.amp.custom_fwd(device_type="cuda", cast_inputs=torch.bfloat16)  # type: ignore
     def forward(ctx, A, delta, B, C, x) -> torch.Tensor:
-
-        if not A.is_contiguous():
-            A = A.contiguous()
-
-        if not C.is_contiguous():
-            C = C.contiguous()
-
-        if not delta.is_contiguous():
-            delta = delta.contiguous()
 
         if not B.is_contiguous():
             B = B.contiguous()
@@ -326,11 +320,6 @@ class MambaRecurrence(Function):
     def backward(ctx, grad_output) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:  # type: ignore
         (A, delta, B, C, x) = ctx.saved_tensors
 
-        if not grad_output.is_contiguous():
-            grad_output = grad_output.contiguous()
-
         A_batch_grad, B_grad, C_grad, delta_grad, x_grad = parallel_scan_backward(A, delta, B, C, x, grad_output)
 
-        return A_batch_grad.to(A.dtype), delta_grad, B_grad, C_grad, x_grad
-
-fused_scan = MambaRecurrence.apply
+        return A_batch_grad, delta_grad, B_grad, C_grad, x_grad
